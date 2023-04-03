be254a88344a2a0e944a8f90899cceff9762b869
Tarun Kumar <tarun.mishra592@gmail.com>
2023-04-03 23:56:20 +0530

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+    return (
+        <div>
+            <h2>Container</h2>
+            <hr/>
+            <MarketingApp/>
+        </div>
+    )
+}
